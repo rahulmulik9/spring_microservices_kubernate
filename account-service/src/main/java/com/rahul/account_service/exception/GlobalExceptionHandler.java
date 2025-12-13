1620b5c5d6b95a36ff382d425801ba028086470a
Rahul Mulik <rahulmulik9@gmail.com>
2025-12-13 22:45:52 +0530

    Added default method for handling validation errors.

--- account-service/src/main/java/com/rahul/account_service/exception/GlobalExceptionHandler.java
+++ account-service/src/main/java/com/rahul/account_service/exception/GlobalExceptionHandler.java
@@ -1,18 +1,37 @@
 package com.rahul.account_service.exception;
 
 import com.rahul.account_service.dto.ErrorResponseDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        Map<String, String> validationErrors = new HashMap<>();
+        List<ObjectError> validationErrorList = ex.getBindingResult().getAllErrors();
+        validationErrorList.forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String validationMsg = error.getDefaultMessage();
+            validationErrors.put(fieldName, validationMsg);
+        });
+        return new ResponseEntity<>(validationErrors, HttpStatus.BAD_REQUEST);
+    }
 
     //Common method if user defined exception not occur then this method will be run and display message
     //to check how this will , remove @AllArgsConstructor from controller, this will lead to failing in bean creation of IAccountservice
