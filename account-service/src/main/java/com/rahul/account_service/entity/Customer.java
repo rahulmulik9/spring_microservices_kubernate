e29d0d4d2091f3e7c6e9a376737923d244cfc481
Rahul Mulik <rahulmulik9@gmail.com>
2025-12-13 09:21:28 +0530

    Added entity classes Added configuration Added data.sql

---
+++ account-service/src/main/java/com/rahul/account_service/entity/Customer.java
@@ -0,0 +1,27 @@
+package com.rahul.account_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer extends  BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="customer_id")
+    private Long customerId;
+
+    private String name;
+
+    private String email;
+
+    @Column(name="mobile_number")
+    private String mobileNumber;
+
+}
