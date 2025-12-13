001797b2602f5b8485134fd8d4c06e776fc09667
Rahul Mulik <rahulmulik9@gmail.com>
2025-12-13 23:21:17 +0530

    Added in @OpenAPIDefinition spring boot application class

--- account-service/src/main/java/com/rahul/account_service/AccountServiceApplication.java
+++ account-service/src/main/java/com/rahul/account_service/AccountServiceApplication.java
@@ -14,21 +14,20 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @OpenAPIDefinition(
         info = @Info(
                 title = "Accounts microservice REST API Documentation",
-                description = "EazyBank Accounts microservice REST API Documentation",
+                description = "Bank Accounts microservice REST API Documentation",
                 version = "v1",
                 contact = @Contact(
-                        name = "Madan Reddy",
-                        email = "tutor@eazybytes.com",
-                        url = "https://www.eazybytes.com"
+                        name = "Rahul Mulik",
+                        email = "rahulmulik9@gmail.com"
                 ),
                 license = @License(
                         name = "Apache 2.0",
-                        url = "https://www.eazybytes.com"
+                        url = "https://www.Apache.com"
                 )
         ),
         externalDocs = @ExternalDocumentation(
-                description =  "EazyBank Accounts microservice REST API Documentation",
-                url = "https://www.eazybytes.com/swagger-ui.html"
+                description =  "Bank Accounts microservice REST API Documentation",
+                url = "https://springdoc.org/"
         )
 )
 public class AccountServiceApplication {
