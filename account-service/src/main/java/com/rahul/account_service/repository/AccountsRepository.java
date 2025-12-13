a495efee9f5ecedab916617ba4f3b26f313804da
Rahul Mulik <rahulmulik9@gmail.com>
2025-12-13 21:50:19 +0530

    Added delete methods in service

--- account-service/src/main/java/com/rahul/account_service/repository/AccountsRepository.java
+++ account-service/src/main/java/com/rahul/account_service/repository/AccountsRepository.java
@@ -9,4 +9,6 @@ import java.util.Optional;
 @Repository
 public interface AccountsRepository extends JpaRepository<Accounts,Long> {
     Optional<Accounts> findByCustomerId(Long customerId);
+
+    void deleteByCustomerId(Long customerId);
 }
