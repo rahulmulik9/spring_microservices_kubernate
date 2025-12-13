790942112382a7df6023a86dc999c1b2ec41b040
Rahul Mulik <rahulmulik9@gmail.com>
2025-12-13 21:44:50 +0530

    Added API for update account information

--- account-service/src/main/java/com/rahul/account_service/controller/AccountController.java
+++ account-service/src/main/java/com/rahul/account_service/controller/AccountController.java
@@ -5,6 +5,7 @@ import com.rahul.account_service.dto.AccountsDto;
 import com.rahul.account_service.dto.CustomerDto;
 import com.rahul.account_service.dto.ResponseDto;
 import com.rahul.account_service.service.IAccountsService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -30,6 +31,20 @@ public class AccountController {
     public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam String mobileNumber) {
         CustomerDto customerDto =  iAccountsService.fetchAccount(mobileNumber);
         return ResponseEntity.status(HttpStatus.OK).body(customerDto);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<ResponseDto> updateAccountDetails(@Valid @RequestBody CustomerDto customerDto) {
+        boolean isUpdated = iAccountsService.updateAccount(customerDto);
+        if(isUpdated) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(AccountsConstants.STATUS_200, AccountsConstants.MESSAGE_200));
+        }else{
+            return ResponseEntity
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(AccountsConstants.STATUS_417, AccountsConstants.MESSAGE_417_UPDATE));
+        }
     }
 
 }
