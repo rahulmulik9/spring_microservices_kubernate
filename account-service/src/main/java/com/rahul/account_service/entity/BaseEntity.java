83be215ce38cec4dfc5e3c7ba3289855d3d0ef67
Rahul Mulik <rahulmulik9@gmail.com>
2025-12-13 23:04:00 +0530

    Added @EntityListeners annotation

--- account-service/src/main/java/com/rahul/account_service/entity/BaseEntity.java
+++ account-service/src/main/java/com/rahul/account_service/entity/BaseEntity.java
@@ -1,12 +1,14 @@
 package com.rahul.account_service.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.*;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -16,6 +18,7 @@ import java.time.LocalDateTime;
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor  //here @Data is not used because sometime toHash method cause some error in spring boot so Getter,Setter, ToString separately annotated.
+@EntityListeners(AuditingEntityListener.class)  //enables automatic population of auditing fields (created/updated date and user) by listening to entity insert and update events.
 public class BaseEntity {
     @CreatedDate
     @Column(updatable = false)
