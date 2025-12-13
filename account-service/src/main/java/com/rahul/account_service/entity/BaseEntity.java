05eb9bee5555e1954b167b6cbb0232a98b224fb2
Rahul Mulik <rahulmulik9@gmail.com>
2025-12-13 22:55:11 +0530

    Added validation BaseEntity

--- account-service/src/main/java/com/rahul/account_service/entity/BaseEntity.java
+++ account-service/src/main/java/com/rahul/account_service/entity/BaseEntity.java
@@ -3,6 +3,10 @@ package com.rahul.account_service.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
@@ -13,15 +17,19 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor  //here @Data is not used because sometime toHash method cause some error in spring boot so Getter,Setter, ToString separately annotated.
 public class BaseEntity {
+    @CreatedDate
     @Column(updatable = false)
     private LocalDateTime createdAt;
 
+    @CreatedBy
     @Column(updatable = false)
     private String createdBy;
 
+    @LastModifiedDate
     @Column(insertable = false)
     private LocalDateTime updatedAt;
 
+    @LastModifiedBy
     @Column(insertable = false)
     private String updatedBy;
 }
