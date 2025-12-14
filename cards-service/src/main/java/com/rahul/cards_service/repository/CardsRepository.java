7062352653e5d695839e81e60f72314a797a6472
Rahul Mulik <rahulmulik9@gmail.com>
2025-12-14 10:57:53 +0530

    Added CardsRepository

---
+++ cards-service/src/main/java/com/rahul/cards_service/repository/CardsRepository.java
@@ -0,0 +1,13 @@
+package com.rahul.cards_service.repository;
+
+import com.rahul.cards_service.entity.Cards;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CardsRepository extends JpaRepository<Cards, Long> {
+
+
+}
