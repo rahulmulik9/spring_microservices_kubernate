821d39b0cb8ebc1b4dded5c96cbb06da34dd02e1
Rahul Mulik <rahulmulik9@gmail.com>
2025-12-22 20:00:10 +0530

    Added API for java home in Card Controller

--- cards-service/src/main/java/com/rahul/cards_service/controller/CardsController.java
+++ cards-service/src/main/java/com/rahul/cards_service/controller/CardsController.java
@@ -17,7 +17,9 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -35,6 +37,9 @@ public class CardsController {
 
     @Value("${build.version}")
     private String buildVersion;
+
+    @Autowired
+    private Environment environment;
 
     @Operation(summary = "Create Card REST API", description = "REST API to create new Card inside EazyBank")
     @ApiResponses({ //api response for swager ui
@@ -136,4 +141,17 @@ public class CardsController {
                 .body(buildVersion);
     }
 
+
+
+    @Operation(summary = "Get Java version", description = "Get Java versions details that is installed into accounts microservice")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "HTTP Status OK"),
+            @ApiResponse(responseCode = "500", description = "HTTP Status Internal Server Error", content = @Content(schema = @Schema(implementation = ErrorResponseDto.class)))
+    })
+    @GetMapping("/java-version")
+    public ResponseEntity<String> getJavaVersion() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(environment.getProperty("JAVA_HOME"));
+    }
 }
